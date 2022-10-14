404e863148679c53fe99f1b68ad76b37d6fa86d2
Nuethera <vaishnavj2124@outlook.com>
2022-10-14 17:55:18 +0530

    pymongo and database setup

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 from flask import Flask, render_template
+import os
+import pymongo
 
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+
+mydb = myclient['portfolio_website']
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+
+pf_pro = mydb['pf_pro']
+
+
+def portfolio_schema(name, desc, repo_link, img_link):
+    return {'name': name, 'desc': desc, 'repo_link': repo_link, 'img_link': img_link}
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return render_template('index.html')
+def hello_world():
+    projs = list(pf_pro.find())
+
+    return render_template('index.html',projs=projs)
 
 
 if __name__ == '__main__':
