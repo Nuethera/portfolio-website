0a2c91dfa7f8143c97138ecf56699cbe5c14e117
Nuethera <vaishnavj2124@outlook.com>
2022-10-17 23:51:28 +0530

    adding form data to database

--- app.py
+++ app.py
@@ -1,33 +1,57 @@
 from flask import Flask, render_template, request, redirect, url_for
 import os
+import datetime as dt
+import pytz
 import pymongo
 
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 
+IST = pytz.timezone('Asia/Kolkata')
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient['portfolio_website']
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
-pf_pro = mydb['pf_pro']
+M_pf_pro = mydb['pf_pro']
+M_contact_form = mydb['contact_form']
 
 
 def portfolio_schema(name, desc, repo_link, img_link):
     return {'name': name, 'desc': desc, 'repo_link': repo_link, 'img_link': img_link}
 
 
+def contact_me(name, email, message, date, time, status='Pending'):
+    return {'name': name, 'email': email, 'message': message, 'date': date, 'time': time,
+            'status': status}
+
+
 @app.route('/')
 def home():
-    projs = list(pf_pro.find())
+    projs = list(M_pf_pro.find())
 
-    return render_template('index.html',projs=projs)
+    return render_template('index.html', projs=projs)
+
 
 @app.route('/contact', methods=['POST'])
 def contact_form():
     if request.method == 'POST':
-        print(request.form.get('name'))
-        print(request.form.get('email'))
-        print(request.form.get('message'))
+        name = request.form.get('name')
+        email = request.form.get('email')
+        message = request.form.get('message')
+        today = dt.datetime.now(IST)
+        time = today.strftime('%H:%M:%S')
+        date = today.strftime('%d/%m/%Y')
+
+        d = contact_me(
+            name=name,
+            email=email,
+            message=message,
+            time=time,
+            date=date
+        )
+        M_contact_form.insert_one(d)
+
         return redirect(url_for('home'))
+
 
 
 if __name__ == '__main__':
